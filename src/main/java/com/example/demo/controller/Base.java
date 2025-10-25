a8bb865ff5f76fc5c1a6c172dd2b6650debfa523
Khafiatov-Bogdan <karlapingus20@gmail.com>
2025-10-26 00:16:50 +0400

    Update Base.java

--- src/main/java/com/example/demo/controller/Base.java
+++ src/main/java/com/example/demo/controller/Base.java
@@ -43,7 +43,7 @@ public class Base {
 
     @GetMapping("/forDima")
     public String Dima() {
-        return "Отдай отмычку, чёрт";
+        return "Пиривет. Я пиздец заёбся, но сделал";
     }
     @GetMapping("/forYarick")
     public String Yarick() {
